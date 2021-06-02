21da9fc482a20d91ba92bedde664564126bf5b75
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-06-02 19:45:55 -0400

    Initialize local Budget DB and basic logs for success, error, upgrade.

--- public/db.js
+++ public/db.js
@@ -2,13 +2,6 @@ let db;
 
 const request = indexedDB.open("BudgetDB", 1);
 
-request.onupgradeneeded = function (event) {
-  // create object store called "BudgetStore" and set autoIncrement to true
-  db = event.target.result;
-  db.createObjectStore("BudgetStore", { autoIncrement: true});
-
-};  
-
 request.onerror = function (event) {
   console.log(`Whoops! There was an error: ${event.target.onerror}`);
 };
@@ -16,8 +9,22 @@ request.onerror = function (event) {
 request.onsuccess = function (event) {
   console.log("success event:", event);
   db = event.target.result;
-
+    let oldVersion = event.oldVersion;
+    let newVersion = event.newVersion || db.version;
+    if (!oldVersion) {
+        console.log(`DB currently running on version ${db.version} - no updates at this time`);
+    } else {
+    console.log(`DB updated from version ${oldVersion} to ${newVersion}`);
+    }
   if (navigator.onLine) {
-    checkDatabase();
+    // checkDatabase();
   }
 };
+
+request.onupgradeneeded = function (event) {
+  // create object store called "BudgetStore" and set autoIncrement to true
+  db = event.target.result;
+  if (!db.objectStoreNames.contains('BudgetStore')) {
+      db.createObjectStore("BudgetStore", { autoIncrement: true});
+  }
+};  
