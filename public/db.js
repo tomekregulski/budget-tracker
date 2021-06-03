10b7696cdbb9a104ee80e71678807e333b73a8d0
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-06-02 21:48:18 -0400

    Successfully save to IndexedDB offline and update back end DB upon reconnection

--- public/db.js
+++ public/db.js
@@ -2,6 +2,14 @@ let db;
 
 const request = indexedDB.open("BudgetDB", 1);
 
+request.onupgradeneeded = function (event) {
+  // create object store called "BudgetStore" and set autoIncrement to true
+  db = event.target.result;
+  if (!db.objectStoreNames.contains('BudgetStore')) {
+      db.createObjectStore("BudgetStore", { autoIncrement: true});
+  }
+};  
+
 request.onerror = function (event) {
   console.log(`Whoops! There was an error: ${event.target.onerror}`);
 };
@@ -21,10 +29,41 @@ request.onsuccess = function (event) {
   }
 };
 
-request.onupgradeneeded = function (event) {
-  // create object store called "BudgetStore" and set autoIncrement to true
-  db = event.target.result;
-  if (!db.objectStoreNames.contains('BudgetStore')) {
-      db.createObjectStore("BudgetStore", { autoIncrement: true});
-  }
-};  
+function saveRecord(record) {
+    console.log('saving the record now...');
+    console.log(record);
+    const transaction = db.transaction(['BudgetStore'], 'readwrite');
+    const offLineStore = transaction.objectStore('BudgetStore');
+    offLineStore.add(record);
+}
+
+function checkDatabase() {
+  console.log('checking the db now...');
+  let transaction = db.transaction(['BudgetStore'], 'readwrite');
+  const store = transaction.objectStore('BudgetStore');
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => response.json())
+        .then((res) => {
+          if (res.length !== 0) {
+            transaction = db.transaction(['BudgetStore'], 'readwrite');
+            const updatedStore = transaction.objectStore('BudgetStore');
+            updatedStore.clear();
+            console.log('clearing the store...');
+          }
+        });
+    }
+  };
+}
+
+window.addEventListener('online', checkDatabase);
--- public/service-worker.js
+++ public/service-worker.js
@@ -4,6 +4,7 @@ const FILES_TO_CACHE = [
   '/manifest.webmanifest',
   '/styles.css',
   '/index.js',
+  '/db.js',
   '/icons/icon-192x192.png',
   '/icons/icon-512x512.png',
 ];
